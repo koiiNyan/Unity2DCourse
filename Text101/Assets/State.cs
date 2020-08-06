7d7cb6ec32afbe2f3587c31d93340820f386ff64
Анастасия Овинникова <anovinnikova@outlook.com>
2020-08-06 16:23:26 +0300

    upd

---
+++ Text101/Assets/State.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="State")]
+
+public class State : ScriptableObject
+{
+    [TextArea(14,14)] [SerializeField] string storyText;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+}
--- Text101/CYOA_Design.md
+++
@@ -1,5 +0,0 @@
-# CYOA DESIGN
-- What is your game theme?
-- What is an image that sums up your game theme?
-- Who is the player?
-- What is the goal?
